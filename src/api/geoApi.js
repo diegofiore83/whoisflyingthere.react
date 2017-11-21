0ae2574cdaa8db7a11c290a6d280dbb9be195f67
Diego Fiore <you@example.com>
2017-11-21 15:27:20 +0000

    configure external local api

--- src/api/geoApi.js
+++ src/api/geoApi.js
@@ -1,22 +1,23 @@
 import fetch from 'isomorphic-fetch';
+import { API_ROOT } from './api-config';
 
 class GeoApi {
   static getCurrencies() {
-    return fetch('http://localhost:80/api/reference/currencies')
+    return fetch(`${API_ROOT}/api/reference/currencies`)
       .then(response => response.json())
       .catch(error => {
       return error;
     });
   }
   static getLocales() {
-    return fetch('http://localhost:80/api/reference/locales')
+    return fetch(`${API_ROOT}/api/reference/locales`)
       .then(response => response.json())
       .catch(error => {
       return error;
     });
   }
   static getMarkets(locale) {
-    return fetch('http://localhost:80/api/reference/markets?locale=' + locale)
+    return fetch(`${API_ROOT}/api/reference/markets?locale=${locale}`)
       .then(response => response.json())
       .catch(error => {
       return error;
